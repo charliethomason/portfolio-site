e249c1af31b99de247cd53082c4b215f66e20176
Charlie Thomason <charlie.thomason@razorfish.com>
2015-07-22 15:17:27 -0500

    added beginner project files

---
+++ gruntfile.js
@@ -0,0 +1,88 @@
+(function() {
+	module.exports = function(grunt) {
+		grunt.initConfig({
+			sass: {
+				build: {
+					options: {
+						style: 'compressed',
+						sourcemap: 'none'
+					},
+					files: {
+						'build/css/main.css': 'src/scss/main.scss'
+					}
+				}
+			},
+			uglify: {
+				options: {
+					mangle: false
+				},
+				build: {
+					files: {
+						'build/js/main.js': ['src/js/scripts.js']
+					}
+				}
+			},
+			assemble: {
+				options: {
+					flatten: true,
+					assets_css: 'css',
+					assets_js: 'js'
+				},
+				prod: {
+					options: {
+						layout: 'src/layouts/layout.hbs',
+						partials: ['src/partials/**/*.hbs'],
+						data: [
+							'src/data/*.json'
+						]
+					},
+					files: {
+						'build/': ['src/pages/*.hbs']
+					}
+				}
+			},
+			copy: {
+				img_build: {
+					files: [{
+						expand: true,
+						cwd: 'src/img/',
+						src: ['**'],
+						dest: 'build/img/'
+					}]
+				}
+			},
+			watch: {
+				css: {
+					files: ['src/scss/*.scss'],
+					tasks: ['sass']
+				},
+				js: {
+					files: ['src/js/scripts.js'],
+					tasks: ['uglify']
+				},
+				html: {
+					files: ['src/layouts/*.hbs', 'src/pages/*.hbs', 'src/partials/*.hbs', 'src/data/*.json'],
+					tasks: ['assemble']
+				},
+				img: {
+					files: ['src/img/*'],
+					tasks: ['copy']
+				}
+			}
+		});
+
+		grunt.loadNpmTasks('grunt-contrib-sass');
+		grunt.loadNpmTasks('grunt-contrib-copy');
+		grunt.loadNpmTasks('grunt-contrib-uglify');
+		grunt.loadNpmTasks('assemble');
+		grunt.loadNpmTasks('grunt-contrib-watch');
+
+		grunt.registerTask('default', [
+			'sass',
+			'assemble',
+			'uglify',
+			'copy',
+			'watch'
+		]);
+	};
+}());
