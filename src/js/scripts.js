ce35885080b691033b740a337ab3897695f1a817
Charlie Thomason <charlie.thomason@razorfish.com>
2015-08-29 13:12:37 -0500

    built out basic lightbox functionality

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,67 @@
+var charlie = charlie || {};
+
+charlie.basics = function() {
+  function hamburger() {
+    $('#hamburger').click(function(e) {
+      e.preventDefault();
+      $('#main-nav ul').toggleClass('shown');
+    });
+  }
+  function init() {
+    hamburger();
+  }
+  init();
+}
+
+charlie.gallery = function() {
+
+  var $lightbox = $('.lightbox'),
+      $lightboxClose = $lightbox.find('.close-btn'),
+      $lightboxImg = $lightbox.find('.lightbox-img img'),
+      $lightboxTitle = $lightbox.find('.lightbox-title'),
+      $lightboxYear = $lightbox.find('.lightbox-year'),
+      $lightboxMedium = $lightbox.find('.lightbox-medium'),
+      $lightboxDescription = $lightbox.find('.lightbox-description');
+
+  function openImage() {
+    $('.galleryImg').click(function(e) {
+      e.preventDefault();
+      var $this = $(this),
+          imgFile = $this.attr('href'),
+          $info = $this.siblings('.info'),
+          imgTitle = $info.find('.title').text(),
+          year = $info.find('.year').text(),
+          medium = $info.find('.medium').text(),
+          description = $info.find('.description').text();
+      $lightbox.show();
+      $lightboxImg.attr('src',imgFile);
+      $lightboxTitle.text(imgTitle);
+      $lightboxYear.text(year);
+      $lightboxMedium.text(medium);
+      $lightboxDescription.text(description);
+    });
+  }
+  function closeLightbox() {
+    $lightboxClose.click(function(e) {
+      e.preventDefault();
+      $lightbox.hide();
+      $lightboxImg.attr('src','');
+      $lightboxTitle.text('');
+      $lightboxYear.text('');
+      $lightboxMedium.text('');
+      $lightboxDescription.text('');
+    });
+  }
+  function init() {
+    openImage();
+    closeLightbox();
+  }
+  init();
+}
+
 $(document).ready(function() {
-  $('#hamburger').click(function(e) {
-    e.preventDefault();
-    $('#main-nav ul').toggleClass('shown');
-  });
+  charlie.basics();
+  if ($('.gallery').length) {
+    charlie.gallery();
+  }
 });
