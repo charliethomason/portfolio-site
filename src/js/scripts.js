5120c2c618d96734178d9add3116b4678366536d
Charlie Thomason <charlie.thomason@razorfish.com>
2015-08-31 19:50:59 -0500

    reworked lightbox into in-page viewer. built out JS functionality

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -16,47 +16,82 @@ charlie.basics = function() {
 charlie.gallery = function() {
 
   var $lightbox = $('.lightbox'),
-      $lightboxClose = $lightbox.find('.close-btn'),
+      $lightboxContent = $lightbox.find('.lightbox-box');
       $lightboxImg = $lightbox.find('.lightbox-img img'),
       $lightboxTitle = $lightbox.find('.lightbox-title'),
       $lightboxYear = $lightbox.find('.lightbox-year'),
       $lightboxMedium = $lightbox.find('.lightbox-medium'),
-      $lightboxDescription = $lightbox.find('.lightbox-description');
+      $lightboxDescription = $lightbox.find('.lightbox-description'),
+      $gallery = $('.gallery > ul');
 
-  function openImage() {
+  function clickEvents() {
     $('.galleryImg').click(function(e) {
       e.preventDefault();
-      var $this = $(this),
-          imgFile = $this.attr('href'),
-          $info = $this.siblings('.info'),
-          imgTitle = $info.find('.title').text(),
-          year = $info.find('.year').text(),
-          medium = $info.find('.medium').text(),
-          description = $info.find('.description').text();
-      $lightbox.show();
-      $lightboxImg.attr('src',imgFile);
-      $lightboxTitle.text(imgTitle);
-      $lightboxYear.text(year);
-      $lightboxMedium.text(medium);
-      $lightboxDescription.text(description);
+      loadImageData(this,openLightbox);
     });
-  }
-  function closeLightbox() {
-    $lightboxClose.click(function(e) {
+    $('.close-btn').click(function(e) {
       e.preventDefault();
-      $lightbox.hide();
-      $lightboxImg.attr('src','');
-      $lightboxTitle.text('');
-      $lightboxYear.text('');
-      $lightboxMedium.text('');
-      $lightboxDescription.text('');
+      closeLightbox();
+    });
+    $('.prev-btn').click(function(e) {
+      e.preventDefault();
+      var $currentImg = $('#' + $lightbox.attr('data-id')),
+          prevImg = $currentImg.parent('li').prev('li').find('.galleryImg');
+      if (prevImg.length) {
+        loadImageData(prevImg);
+      }
+    });
+    $('.next-btn').click(function(e) {
+      e.preventDefault();
+      var $currentImg = $('#' + $lightbox.attr('data-id')),
+          nextImg = $currentImg.parent('li').next('li').find('.galleryImg');
+      if (nextImg.length) {
+        loadImageData(nextImg);
+      }
     });
   }
+
+  function loadImageData(img,callback) {
+    var $image = $(img),
+        imgFile = $image.attr('href'),
+        $info = $image.siblings('.info'),
+        imgTitle = $info.find('.title').text(),
+        year = $info.find('.year').text(),
+        medium = $info.find('.medium').text(),
+        description = $info.find('.description').text(),
+        imgID = $image.attr('id');
+    $lightboxImg.attr('src',imgFile);
+    $lightboxTitle.text(imgTitle);
+    $lightboxYear.text(year);
+    $lightboxMedium.text(medium);
+    $lightboxDescription.text(description);
+    $lightbox.attr('data-id',imgID);
+    if (callback) {
+      callback();
+    }
+  }
+
+  function openLightbox() {
+    $gallery.hide();
+    $lightbox.show();
+  }
+
+  function closeLightbox() {
+    $lightbox.hide();
+    $lightbox.attr('data-id','');
+    $lightboxImg.attr('src','');
+    $lightboxTitle.text('');
+    $lightboxYear.text('');
+    $lightboxMedium.text('');
+    $lightboxDescription.text('');
+    $gallery.show();
+  }
+
   function init() {
-    openImage();
-    closeLightbox();
+    clickEvents();
   }
   init();
+
 }
 
 $(document).ready(function() {
