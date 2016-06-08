7395f455578304827ad31551d124f72767001e4e
Charlie Thomason <charlie.thomason@razorfish.com>
2016-06-08 15:51:09 -0500

    removed/commented-out CSS & JS for old gallery functions

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -44,153 +44,153 @@ charlie.gallery = function() {
     //     window.open(page,'_self');
     //   }
     // });
-    $('.close-btn').click(function(e) {
-      e.preventDefault();
-      closeLightbox();
-    });
-    $('.prev-btn').click(function(e) {
-      e.preventDefault();
-      loadPrevImg();
-    });
-    $('.next-btn').click(function(e) {
-      e.preventDefault();
-      loadNextImg();
-    });
+    // $('.close-btn').click(function(e) {
+    //   e.preventDefault();
+    //   closeLightbox();
+    // });
+    // $('.prev-btn').click(function(e) {
+    //   e.preventDefault();
+    //   loadPrevImg();
+    // });
+    // $('.next-btn').click(function(e) {
+    //   e.preventDefault();
+    //   loadNextImg();
+    // });
     $('.sort-method a').click(function(e) {
       e.preventDefault();
       gallerySort(this);
     });
-    $('.lightbox-img').click(function(e) {
-      e.preventDefault();
-      fixedLightbox();
-    });
+    // $('.lightbox-img').click(function(e) {
+    //   e.preventDefault();
+    //   fixedLightbox();
+    // });
   }
 
-  function loadImageData(img,callback) {
-    var $image = $(img),
-        imgFile = $image.attr('href'),
-        imgID = $image.attr('data-id'),
-        $info = $image.siblings('.info'),
-        imgTitle = $info.find('.title').text(),
-        year = $info.find('.year').text(),
-        medium = $info.find('.medium').text(),
-        description = $info.find('.description').html();
-    $('.grid:visible').hide();
-    $('.sort-method:visible').hide();
-    $lightboxContent.hide();
-    $loading.show();
-    $lightboxImg.attr('src',imgFile);
-    $lightboxImg.attr('alt',imgTitle);
-    $lightboxTitle.text(imgTitle);
-    $lightboxMedium.text(medium);
-    $lightboxDescription.html(description);
-    $lightbox.attr('data-id',imgID);
-    if ($info.find('.location').length) {
-      var imgLocation = $info.find('.location').text();
-      $lightboxLocation.text(imgLocation);
-      $lightboxLocation.attr('href','http://maps.google.com/maps?q='+imgLocation);
-      $lightboxLocationWrap.show();
-    }
-    if ($info.find('.month').length) {
-      var month = $info.find('.month').text();
-      var monthName = monthNames[(month - 1)];
-      $lightboxYear.text(monthName + ' ' + year);
-    } else {
-      $lightboxYear.text(year);
-    }
-    location.href = '#' + imgID;
-    if (callback) {
-      callback();
-    }
-  }
+  // function loadImageData(img,callback) {
+  //   var $image = $(img),
+  //       imgFile = $image.attr('href'),
+  //       imgID = $image.attr('data-id'),
+  //       $info = $image.siblings('.info'),
+  //       imgTitle = $info.find('.title').text(),
+  //       year = $info.find('.year').text(),
+  //       medium = $info.find('.medium').text(),
+  //       description = $info.find('.description').html();
+  //   $('.grid:visible').hide();
+  //   $('.sort-method:visible').hide();
+  //   $lightboxContent.hide();
+  //   $loading.show();
+  //   $lightboxImg.attr('src',imgFile);
+  //   $lightboxImg.attr('alt',imgTitle);
+  //   $lightboxTitle.text(imgTitle);
+  //   $lightboxMedium.text(medium);
+  //   $lightboxDescription.html(description);
+  //   $lightbox.attr('data-id',imgID);
+  //   if ($info.find('.location').length) {
+  //     var imgLocation = $info.find('.location').text();
+  //     $lightboxLocation.text(imgLocation);
+  //     $lightboxLocation.attr('href','http://maps.google.com/maps?q='+imgLocation);
+  //     $lightboxLocationWrap.show();
+  //   }
+  //   if ($info.find('.month').length) {
+  //     var month = $info.find('.month').text();
+  //     var monthName = monthNames[(month - 1)];
+  //     $lightboxYear.text(monthName + ' ' + year);
+  //   } else {
+  //     $lightboxYear.text(year);
+  //   }
+  //   location.href = '#' + imgID;
+  //   if (callback) {
+  //     callback();
+  //   }
+  // }
 
-  function openLightbox() {
-    $lightbox.imagesLoaded(function() {
-      $loading.hide();
-      $lightboxContent.show();
-      $('.lightbox:hidden').show();
-    });
-  }
+  // function openLightbox() {
+  //   $lightbox.imagesLoaded(function() {
+  //     $loading.hide();
+  //     $lightboxContent.show();
+  //     $('.lightbox:hidden').show();
+  //   });
+  // }
 
-  function closeLightbox() {
-    $lightbox.hide();
-    $lightbox.attr('data-id','');
-    $lightboxImg.attr('src','');
-    $lightboxImg.attr('alt','');
-    $lightboxTitle.text(defaultTitle);
-    $lightboxYear.text('');
-    $lightboxMedium.text('');
-    $lightboxDescription.text('');
-    $lightboxLocation.text('');
-    $lightboxLocation.attr('href','http://maps.google.com/maps?q=');
-    $lightboxLocationWrap.hide();
-    location.href = '#';
-    $sort.show();
-    $grid.show();
-  }
+  // function closeLightbox() {
+  //   $lightbox.hide();
+  //   $lightbox.attr('data-id','');
+  //   $lightboxImg.attr('src','');
+  //   $lightboxImg.attr('alt','');
+  //   $lightboxTitle.text(defaultTitle);
+  //   $lightboxYear.text('');
+  //   $lightboxMedium.text('');
+  //   $lightboxDescription.text('');
+  //   $lightboxLocation.text('');
+  //   $lightboxLocation.attr('href','http://maps.google.com/maps?q=');
+  //   $lightboxLocationWrap.hide();
+  //   location.href = '#';
+  //   $sort.show();
+  //   $grid.show();
+  // }
 
-  function loadPrevImg() {
-    var $currentImg = $('.galleryImg[data-id="' + $lightbox.attr('data-id') + '"]'),
-        prevImg = $currentImg.parent('li').prev('li.galleryImg-li').find('.galleryImg');
-    if (prevImg.length) {
-      loadImageData(prevImg,openLightbox);
-    } else {
-      var lastImg = $('.galleryImg:last');
-      loadImageData(lastImg,openLightbox);
-    }
-  }
+  // function loadPrevImg() {
+  //   var $currentImg = $('.galleryImg[data-id="' + $lightbox.attr('data-id') + '"]'),
+  //       prevImg = $currentImg.parent('li').prev('li.galleryImg-li').find('.galleryImg');
+  //   if (prevImg.length) {
+  //     loadImageData(prevImg,openLightbox);
+  //   } else {
+  //     var lastImg = $('.galleryImg:last');
+  //     loadImageData(lastImg,openLightbox);
+  //   }
+  // }
 
-  function loadNextImg() {
-    var $currentImg = $('.galleryImg[data-id="' + $lightbox.attr('data-id') + '"]'),
-        nextImg = $currentImg.parent('li').next('li.galleryImg-li').find('.galleryImg');
-    if (nextImg.length) {
-      loadImageData(nextImg,openLightbox);
-    } else {
-      var firstImg = $('.galleryImg:first');
-      loadImageData(firstImg,openLightbox);
-    }
-  }
+  // function loadNextImg() {
+  //   var $currentImg = $('.galleryImg[data-id="' + $lightbox.attr('data-id') + '"]'),
+  //       nextImg = $currentImg.parent('li').next('li.galleryImg-li').find('.galleryImg');
+  //   if (nextImg.length) {
+  //     loadImageData(nextImg,openLightbox);
+  //   } else {
+  //     var firstImg = $('.galleryImg:first');
+  //     loadImageData(firstImg,openLightbox);
+  //   }
+  // }
 
-  function hashOpen() {
-    if (window.location.hash) {
-      var urlHash = window.location.hash.replace('#','');
-      if (urlHash === 'random') {
-        randomArt();
-      } else {
-        var img = $('.galleryImg[data-id="' + urlHash + '"]');
-        loadImageData(img,openLightbox);
-      }
-    }
-  }
+  // function hashOpen() {
+  //   if (window.location.hash) {
+  //     var urlHash = window.location.hash.replace('#','');
+  //     if (urlHash === 'random') {
+  //       randomArt();
+  //     } else {
+  //       var img = $('.galleryImg[data-id="' + urlHash + '"]');
+  //       loadImageData(img,openLightbox);
+  //     }
+  //   }
+  // }
 
-  function keyboardNav() {
-    $(document).keydown(function(e) {
-      if ($lightbox.is(':visible')) {
-        switch(e.keyCode) {
-          case 37:
-            e.preventDefault();
-            loadPrevImg();
-            break;
-          case 39:
-            e.preventDefault();
-            loadNextImg();
-            break;
-        }
-      }
-      if ($('.lightbox-img').hasClass('fixed-lightbox') && e.keyCode == 27) {
-        e.preventDefault();
-        fixedLightbox();
-      }
-    });
-  }
+  // function keyboardNav() {
+  //   $(document).keydown(function(e) {
+  //     if ($lightbox.is(':visible')) {
+  //       switch(e.keyCode) {
+  //         case 37:
+  //           e.preventDefault();
+  //           loadPrevImg();
+  //           break;
+  //         case 39:
+  //           e.preventDefault();
+  //           loadNextImg();
+  //           break;
+  //       }
+  //     }
+  //     if ($('.lightbox-img').hasClass('fixed-lightbox') && e.keyCode == 27) {
+  //       e.preventDefault();
+  //       fixedLightbox();
+  //     }
+  //   });
+  // }
 
-  function randomArt() {
-    var max = $('.galleryImg').length,
-        min = 0,
-        imageNum = Math.round(Math.random() * (max - min)) + min;
-        img = $('.galleryImg').eq(imageNum);
-    loadImageData(img,openLightbox);
-  }
+  // function randomArt() {
+  //   var max = $('.galleryImg').length,
+  //       min = 0,
+  //       imageNum = Math.round(Math.random() * (max - min)) + min;
+  //       img = $('.galleryImg').eq(imageNum);
+  //   loadImageData(img,openLightbox);
+  // }
 
   function gallerySort(link) {
     if (!$(link).hasClass('active')) {
@@ -230,14 +230,14 @@ charlie.gallery = function() {
     }
   }
 
-  function fixedLightbox() {
-    $('.lightbox-img').toggleClass('fixed-lightbox');
-  }
+  // function fixedLightbox() {
+  //   $('.lightbox-img').toggleClass('fixed-lightbox');
+  // }
 
   function init() {
     clickEvents();
-    hashOpen();
-    keyboardNav();
+    // hashOpen();
+    // keyboardNav();
   }
   init();
 
